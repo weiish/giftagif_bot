2b5eca8296306a505b4d44d7c9168ed910caa492
Wei Shi <shixwei@gmail.com>
2020-07-23 18:41:42 -0500

    Basic Bot Setup

---
+++ GiftAGIfBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiftAGIfBot
+{
+    public class Program
+    {
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+            _client.MessageReceived += MessageReceived;            
+
+            // Remember to keep token private or to read it from an 
+            // external source! In this case, we are reading the token 
+            // from an environment variable. If you do not know how to set-up
+            // environment variables, you may find more information on the 
+            // Internet or by using other methods such as reading from 
+            // a configuration.
+
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("GiftAGifBotToken", EnvironmentVariableTarget.User));
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+            {
+                await message.Channel.SendMessageAsync("Pong!");
+            }
+        }
+
+    }
+}
